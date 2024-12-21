4992c3bebd0250d5e8737f0b6a07abb5ae8b68f3
BaselMohab <baselmohab99@gmail.com>
2024-12-21 22:12:05 +0200

    add test

---
+++ jest.config.ts
@@ -0,0 +1,7 @@
+module.exports = {
+  moduleNameMapper: {
+    '^@testing-library/react-hooks$': '<rootDir>/node_modules/@testing-library/react-hooks',
+  },
+  moduleDirectories: ['node_modules', 'src'],
+  testEnvironment: 'jsdom',
+};
---
+++ src/jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/src/$1',
+      '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    },
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  }
