d855a8e04332647e21358980133bbb4c94749eaf
BaselMohab <baselmohab99@gmail.com>
2024-12-22 00:22:10 +0200

    add error

--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -1,12 +1,14 @@
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import error from '../../../public/error.gif'
 export default function ErrorComponent() {
   const { t } = useTranslation();
   return (
-    <article >
+    <article className='flex flex-col gap-5 justify-center items-start'>
       <h1>{t('pages.error.header')}</h1>
+      <img src={error} alt="Error" />
       <p>{t('pages.error.message')}</p>
-      <Link to={'/'}>{t('pages.error.cta')}</Link>
+      <Link to={'/'} className="bg-searchBtn text-white py-2 px-4 rounded-md">{t('pages.error.cta')}</Link>
     </article>
   );
 }
